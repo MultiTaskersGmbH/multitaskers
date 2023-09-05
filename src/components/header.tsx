f9d717e122666e0a60f845ef57b52ca47f49b937
Andreas Wagner <info@andreaswagner.dev>
2023-09-05 17:12:48 +0100

    add header content

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,125 +9,124 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import logo from "public/logo.png";
 
-const navigation = [
-    { name: "Product", href: "#" },
-    { name: "Features", href: "#" },
-    { name: "Marketplace", href: "#" },
-    { name: "Company", href: "#" },
-];
+import dictionarie from "@/dictionaries/header.json";
 
 export default function Header() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    return (
-        <header className="bg-white">
-            <nav
-                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-                aria-label="Global"
+  const navigation = dictionarie.navigation;
+
+  return (
+    <header className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 sm:px-8">
+        <nav
+          className="flex justify-between py-4 border-b border-gray-400"
+          aria-label="Global"
+        >
+          <Link
+            href="/"
+            className="-m-1.5 p-1.5"
+          >
+            <span className="sr-only">{dictionarie.logoSrOnly["en-GB"]}</span>
+            <Image
+              className="h-8 w-auto"
+              src={logo}
+              alt={dictionarie.logoImageAlt["en-GB"]}
+              width={500}
+              height={500}
+            />
+          </Link>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
             >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                className="h-6 w-6"
+                aria-hidden="true"
+              />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:items-center lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link
+                key={item.name["en-GB"]}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name["en-GB"]}
+              </Link>
+            ))}
+            <Link
+              href={dictionarie.linkPrimary.href}
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {dictionarie.linkPrimary.name["en-GB"]}
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link
+              href="/"
+              className="-m-1.5 p-1.5"
+            >
+              <span className="sr-only">{dictionarie.logoSrOnly["en-GB"]}</span>
+              <Image
+                className="h-8 w-auto"
+                src={logo}
+                alt={dictionarie.logoImageAlt["en-GB"]}
+              />
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">
+                {dictionarie.menuCloseSrOnly["en-GB"]}
+              </span>
+              <XMarkIcon
+                className="h-6 w-6"
+                aria-hidden="true"
+              />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name["en-GB"]}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name["en-GB"]}
+                  </Link>
+                ))}
+              </div>
+              <div>
                 <Link
-                    href="/"
-                    className="-m-1.5 p-1.5"
+                  href={dictionarie.linkPrimary.href}
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    <span className="sr-only">
-                        MultiTaskers Language School
-                    </span>
-                    <Image
-                        className="h-8 w-auto"
-                        src={logo}
-                        alt=""
-                        width={500}
-                        height={500}
-                    />
+                  {dictionarie.linkPrimary.name["en-GB"]}
                 </Link>
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                        onClick={() => setMobileMenuOpen(true)}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                        />
-                    </button>
-                </div>
-                <div className="hidden lg:flex lg:gap-x-12">
-                    {navigation.map((item) => (
-                        <Link
-                            key={item.name}
-                            href={item.href}
-                            className="text-sm font-semibold leading-6 text-gray-900"
-                        >
-                            {item.name}
-                        </Link>
-                    ))}
-                    <Link
-                        href="#"
-                        className="text-sm font-semibold leading-6 text-gray-900"
-                    >
-                        Log in <span aria-hidden="true">&rarr;</span>
-                    </Link>
-                </div>
-            </nav>
-            <Dialog
-                as="div"
-                className="lg:hidden"
-                open={mobileMenuOpen}
-                onClose={setMobileMenuOpen}
-            >
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
-                        <Link
-                            href="#"
-                            className="-m-1.5 p-1.5"
-                        >
-                            <span className="sr-only">Your Company</span>
-                            <Image
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
-                            />
-                        </Link>
-                        <button
-                            type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                            onClick={() => setMobileMenuOpen(false)}
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                            />
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                    <Link
-                                        key={item.name}
-                                        href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        {item.name}
-                                    </Link>
-                                ))}
-                            </div>
-                            <div className="py-6">
-                                <Link
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Log in
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog.Panel>
-            </Dialog>
-        </header>
-    );
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
 }
