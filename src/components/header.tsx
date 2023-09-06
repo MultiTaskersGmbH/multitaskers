8ce989afee257c17c6864f4210d6e8417213fb58
Andreas Wagner <info@andreaswagner.dev>
2023-09-06 11:31:59 +0100

    refactor header navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-import dictionarie from "@/dictionaries/header.json";
+import dictionarie from "@/dictionaries/navigation.json";
 
 const logo = "/logo.png";
 
@@ -18,7 +18,7 @@ export default function Header() {
 
   return (
     <header className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 sm:px-8">
+      <div className="mx-auto max-w-7xl px-6 md:px-10">
         <nav
           className="flex justify-between py-4 border-b border-gray-400"
           aria-label="Global"
@@ -42,7 +42,9 @@ export default function Header() {
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
-              <span className="sr-only">Open main menu</span>
+              <span className="sr-only">
+                {dictionarie.menuOpenSrOnly["en-GB"]}
+              </span>
               <Bars3Icon
                 className="h-6 w-6"
                 aria-hidden="true"
@@ -50,20 +52,33 @@ export default function Header() {
             </button>
           </div>
           <div className="hidden lg:flex lg:items-center lg:gap-x-12">
-            {navigation.map((item) => (
-              <Link
-                key={item.name["en-GB"]}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                {item.name["en-GB"]}
-              </Link>
-            ))}
             <Link
-              href={dictionarie.linkPrimary.href}
+              key={dictionarie.navigation.homepage.name["en-GB"]}
+              href={dictionarie.navigation.homepage.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {dictionarie.navigation.homepage.name["en-GB"]}
+            </Link>
+            <Link
+              key={dictionarie.navigation.faq.name["en-GB"]}
+              href={dictionarie.navigation.faq.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {dictionarie.navigation.faq.name["en-GB"]}
+            </Link>
+            <Link
+              key={dictionarie.navigation.teachers.name["en-GB"]}
+              href={dictionarie.navigation.teachers.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {dictionarie.navigation.teachers.name["en-GB"]}
+            </Link>
+            <Link
+              key={dictionarie.navigation.onlineCourses.name["en-GB"]}
+              href={dictionarie.navigation.onlineCourses.href}
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              {dictionarie.linkPrimary.name["en-GB"]}
+              {dictionarie.navigation.onlineCourses.name["en-GB"]}
             </Link>
           </div>
         </nav>
@@ -83,7 +98,7 @@ export default function Header() {
             >
               <span className="sr-only">{dictionarie.logoSrOnly["en-GB"]}</span>
               <Image
-                className="h-8 w-auto"
+                className="h-10 w-auto"
                 src={logo}
                 alt={dictionarie.logoImageAlt["en-GB"]}
                 width={500}
@@ -107,22 +122,35 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name["en-GB"]}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name["en-GB"]}
-                  </Link>
-                ))}
+                <Link
+                  key={dictionarie.navigation.homepage.name["en-GB"]}
+                  href={dictionarie.navigation.homepage.href}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  {dictionarie.navigation.homepage.name["en-GB"]}
+                </Link>
+                <Link
+                  key={dictionarie.navigation.faq.name["en-GB"]}
+                  href={dictionarie.navigation.faq.href}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  {dictionarie.navigation.faq.name["en-GB"]}
+                </Link>
+                <Link
+                  key={dictionarie.navigation.teachers.name["en-GB"]}
+                  href={dictionarie.navigation.teachers.href}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  {dictionarie.navigation.teachers.name["en-GB"]}
+                </Link>
               </div>
               <div>
                 <Link
-                  href={dictionarie.linkPrimary.href}
+                  key={dictionarie.navigation.onlineCourses.name["en-GB"]}
+                  href={dictionarie.navigation.onlineCourses.href}
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  {dictionarie.linkPrimary.name["en-GB"]}
+                  {dictionarie.navigation.onlineCourses.name["en-GB"]}
                 </Link>
               </div>
             </div>
