34eadc3ec8a0dda66ea12fbbe23add9891ad7574
Andreas Wagner <info@andreaswagner.dev>
2023-10-06 10:42:33 +0100

    add homepage styles

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -17,15 +17,15 @@ export default async function Page({
       <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
         <div className="md:grid md:grid-cols-2 md:gap-6">
           {/* hero-section */}
-          <section className="py-24">
+          <section className="pb-12 pt-24">
             <h1 className="mb-4 text-5xl">{home.hero.heading}</h1>
             <p className="mb-8">{home.hero.description}</p>
             <Button href={`/${lang}/courses`}>{home.hero.linkButton}</Button>
           </section>
           <div className="md:sticky md:top-4">
             <Image
-              className="w-full"
-              src="/images/placeholder.jpeg"
+              className="w-full rounded-md"
+              src="/images/hero-image.jpeg"
               alt={home.hero.imageAlt}
               width={618}
               height={500}
@@ -34,9 +34,9 @@ export default async function Page({
 
           {/* course-section */}
           <section className="pt-24">
-            <h2>{home.courses.heading}</h2>
-            <p>{home.courses.title}</p>
-            <p>{home.courses.description}</p>
+            <h2 className="mb-2">{home.courses.heading}</h2>
+            <p className="mb-4 text-4xl">{home.courses.title}</p>
+            <p className="mb-8">{home.courses.description}</p>
             <Button href={`/${lang}/courses`}>{home.courses.linkButton}</Button>
           </section>
         </div>
@@ -44,24 +44,29 @@ export default async function Page({
 
       {/* benefits-section */}
       <section className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
-        <h3>{home.benefits.heading}</h3>
-        <div>
+        <h3 className="mb-2">{home.benefits.heading}</h3>
+        <div className="grid gap-6">
           {home.benefits.list.map((benefit) => (
-            <div key={benefit.heading}>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-6 w-6 shrink-0"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <h4>{benefit.heading}</h4>
+            <div
+              key={benefit.heading}
+              className="rounded-md border border-teal-300 bg-white p-4 "
+            >
+              <div className="mb-2 flex gap-3">
+                <div className="shrink-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-6 w-6 shrink-0"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <h4 className="font-bold">{benefit.heading}</h4>
               </div>
               <p>{benefit.description}</p>
             </div>
@@ -70,14 +75,17 @@ export default async function Page({
       </section>
 
       {/* team-section */}
-      <section>
-        <div>
-          <h2>{home.team.heading}</h2>
-          <p>{home.team.title}</p>
-          <p>{home.team.description}</p>
-          <Button href={`/${lang}/team`}>{home.team.linkButton}</Button>
+      <section className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+        <div className="pb-12">
+          <h2 className="mb-2">{home.team.heading}</h2>
+          <p className="mb-4 text-4xl">{home.team.title}</p>
+          <p className="mb-8">{home.team.description}</p>
+          <Button href={`/${lang}/team`} variant="secondary">
+            {home.team.linkButton}
+          </Button>
         </div>
         <Image
+          className="w-full rounded-md"
           src="/images/team.jpeg"
           alt={home.team.imageAlt}
           width={2561}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,9 +3,9 @@ import clsx from "clsx";
 
 const variantStyles = {
   primary:
-    "bg-teal-600 font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2",
+    "bg-teal-600 text-sm font-semibold text-white shadow-sm hover:bg-teal-500",
   secondary:
-    "bg-white font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50",
+    "bg-white text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50",
 };
 
 type ButtonProps = {
@@ -21,13 +21,13 @@ export default function Button({
   ...props
 }: ButtonProps) {
   className = clsx(
-    "inline-flex items-center gap-2 justify-center rounded-md px-3.5 py-2.5 transition active:transition-none focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+    "inline-flex items-center justify-center gap-2 rounded-md px-3.5 py-2.5 transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 active:transition-none",
     variantStyles[variant],
     className,
   );
 
   return typeof props.href === "undefined" ? (
-    <button className={className} {...props} />
+    <button className="{className}" {...props} />
   ) : (
     <Link className={className} {...props} />
   );
