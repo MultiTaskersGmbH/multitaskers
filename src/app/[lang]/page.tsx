c1cc7e6fce46441ad3838fe8bf3b1398533fd662
Andreas Wagner <info@andreaswagner.dev>
2023-10-06 12:15:58 +0100

    add home styleing

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -17,12 +17,12 @@ export default async function Page({
       <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
         <div className="md:grid md:grid-cols-2 md:gap-6">
           {/* hero-section */}
-          <section className="pb-12 pt-24">
+          <section className="pb-12 pt-24 md:max-w-lg md:pb-0 lg:max-w-xl">
             <h1 className="mb-4 text-5xl">{home.hero.heading}</h1>
             <p className="mb-8">{home.hero.description}</p>
             <Button href={`/${lang}/courses`}>{home.hero.linkButton}</Button>
           </section>
-          <div className="md:sticky md:top-4">
+          <div className="md:sticky md:top-4 ">
             <Image
               className="w-full rounded-md"
               src="/images/hero-image.jpeg"
@@ -33,7 +33,7 @@ export default async function Page({
           </div>
 
           {/* course-section */}
-          <section className="pt-24">
+          <section className="pt-24  md:max-w-lg md:pt-0 lg:max-w-xl">
             <h2 className="mb-2">{home.courses.heading}</h2>
             <p className="mb-4 text-4xl">{home.courses.title}</p>
             <p className="mb-8">{home.courses.description}</p>
@@ -45,7 +45,7 @@ export default async function Page({
       {/* benefits-section */}
       <section className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
         <h3 className="mb-2">{home.benefits.heading}</h3>
-        <div className="grid gap-6">
+        <div className="grid gap-6 md:grid-cols-2">
           {home.benefits.list.map((benefit) => (
             <div
               key={benefit.heading}
@@ -57,11 +57,11 @@ export default async function Page({
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    className="h-6 w-6 shrink-0"
+                    className="h-6 w-6"
                   >
                     <path
                       fillRule="evenodd"
-                      d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                      d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
                       clipRule="evenodd"
                     />
                   </svg>
@@ -76,7 +76,7 @@ export default async function Page({
 
       {/* team-section */}
       <section className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
-        <div className="pb-12">
+        <div className="pb-12 md:max-w-lg lg:max-w-xl">
           <h2 className="mb-2">{home.team.heading}</h2>
           <p className="mb-4 text-4xl">{home.team.title}</p>
           <p className="mb-8">{home.team.description}</p>
